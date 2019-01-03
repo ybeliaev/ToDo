7471896646f1aea374e52ee86c0c36c30e93c444
ybeliaev <ybeliaev@gmail.com>
2019-01-03 17:54:38 +0200

    добавление массива как свойства компонента

--- src/index.js
+++ src/index.js
@@ -8,11 +8,17 @@ import ToDoList from './components/todo-list';
 
 const App = () => {
 
+    const todoData = [
+        {label: 'Drink Coffee', important : false},
+        {label: 'Make Awesome App', important : true},
+        {label: 'Have a lunch', important : false}
+    ]
+
     return (
         <div>
             <AppHeader />
             <SearchPanel />
-            <ToDoList />
+            <ToDoList todos={todoData}/>
         </div>
     )
 }
