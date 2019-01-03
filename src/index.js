817fd65db9a5d1d2b77e8806c79465a6c89db43e
ybeliaev <ybeliaev@gmail.com>
2019-01-03 13:18:25 +0200

    создание первых компонентов

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const ToDoList = () => {
+    return (
+        <ul>
+            <li>Learn React.js</li>
+            <li>Build Awesome App</li>
+        </ul>
+    )
+}
+const AppHeader = () => {
+    return ( <h1>ToDO List</h1> )
+}
+const SearchPanel = () => {
+    return ( <input placeholder='search'/> )
+}
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <ToDoList />
+        </div>
+    )
+}
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
